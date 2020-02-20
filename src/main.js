862e65206f00d314266d288f0340594e90d1a61e
SuperLukedin <yp54@njit.edu>
2020-02-19 21:26:16 -0500

    add babel-polyfill

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import VueResource from 'vue-resource'
 import vuetify from './plugins/vuetify';
+
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
